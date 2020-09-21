02c98a71df6a71c58cce845c516851d56c9d6619
Aryan Arora <aryanxarora@gmail.com>
2020-09-22 00:41:50 +0800

    typewriter effect homepage

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
   })
+
+var typed = new Typed('.type', {
+  strings: [
+    'ALPHA PHI BETA', 'UP COLLEGE OF LAW'],
+    smartBackspace: true,
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
