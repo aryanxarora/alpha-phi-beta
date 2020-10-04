8107ffca1a2ead3d4abf443fc3da53bb2c9c554c
Aryan Arora <aryanxarora@gmail.com>
2020-10-04 20:24:09 +0800

    section 2,4,5

--- js/script.js
+++ js/script.js
@@ -14,4 +14,16 @@ var typed = new Typed('.type', {
     loop: true
 });
 
-$('.num').counterUp({delay: 10, timeout: 1000});
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
